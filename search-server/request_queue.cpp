336f865cc344c2fa5d9cea27af0748ddf2983c4f
fartuner <91907999+fartuner@users.noreply.github.com>
2025-01-17 22:29:23 +0300

    Update request_queue.cpp

--- search-server/request_queue.cpp
+++ search-server/request_queue.cpp
@@ -23,3 +23,15 @@ void RequestQueue::AddRequest(int results_num) {
         ++no_results_requests_;
     }
 }
+
+std::vector<Document> RequestQueue::AddFindRequest(const std::string& raw_query, DocumentStatus status) {
+    const auto result = search_server_.FindTopDocuments(raw_query, status);
+    AddRequest(result.size());
+    return result;
+}
+
+std::vector<Document> RequestQueue::AddFindRequest(const std::string& raw_query) {
+    const auto result = search_server_.FindTopDocuments(raw_query);
+    AddRequest(result.size());
+    return result;
+}
