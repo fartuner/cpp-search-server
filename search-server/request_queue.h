74b9a14699f82b53a40f4740b6fbd229efa949ef
fartuner <91907999+fartuner@users.noreply.github.com>
2025-01-17 22:22:34 +0300

    Update request_queue.h

--- search-server/request_queue.h
+++ search-server/request_queue.h
@@ -7,21 +7,13 @@ class RequestQueue {
 public:
     explicit RequestQueue(const SearchServer& search_server);
 
+    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentStatus status);
+    
+    std::vector<Document> AddFindRequest(const std::string& raw_query);
+
     template <typename DocumentPredicate>
     std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate) {
         const auto result = search_server_.FindTopDocuments(raw_query, document_predicate);
-        AddRequest(result.size());
-        return result;
-    }
-
-    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentStatus status) {
-        const auto result = search_server_.FindTopDocuments(raw_query, status);
-        AddRequest(result.size());
-        return result;
-    }
-
-    std::vector<Document> AddFindRequest(const std::string& raw_query) {
-        const auto result = search_server_.FindTopDocuments(raw_query);
         AddRequest(result.size());
         return result;
     }
